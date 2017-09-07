9c11c957abffaa6fd76e95303ab0ba8734b9cc82
miiraajung@gmail.com <miiraajung@gmail.com>
2017-09-07 21:07:20 +0900

    calc2.js 모듈 파일을 불러들이게 추가

--- books/doitnodejs/example1/02_5.js
+++ books/doitnodejs/example1/02_5.js
@@ -1,7 +1,8 @@
  //더하기 함수를 모듈로 분리한 calc.js 모듈 파일을 불러들임 메인파일
+ //곱하기 함수를 모률로 분리한 calc2.js 모듈 파일을 불러들이는 메인파일
  
 var calc = require('./calc');
 console.log('모듈로 분리한 후 calc.add함수 : %d', calc.add(10, 10));
 
-var calc2 = require('./calc');
-console.log('모듈로 분리한 후 calc.add함수 : %d', calc2.add(10,10));
+var calc2 = require('./calc2');
+console.log('모듈로 분리한 후 calc2.mul함수 : %d', calc2.mul(10,10));
